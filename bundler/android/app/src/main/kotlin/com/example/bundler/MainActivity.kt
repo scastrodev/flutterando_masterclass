89bf0ca036237a0d42701ea2b971966d10de37b2
Samuel Castro <samuel.castrolim4@gmail.com>
2022-11-14 17:56:42 -0300

    Initializing the project.

---
+++ bundler/android/app/src/main/kotlin/com/example/bundler/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bundler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ exercises/exercise_02_animations/android/app/src/main/kotlin/com/example/exercise_02_animations/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.exercise_02_animations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
